7f6c757e73702397661dd54e792051c268c124a2
Eyshield <samy.amaouche008@gmail.com>
2025-10-22 18:56:08 +0200

    initiate the landing page

--- Frontend/ChatInterface/src/app/Widget/landing-page/landing-page.ts
+++ Frontend/ChatInterface/src/app/Widget/landing-page/landing-page.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  standalone: true,
+  imports: [FontAwesomeModule],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
-export class LandingPage {}
+export class LandingPage {
+  faTelegram = faTelegram;
+  menuDrop: boolean = false;
+}
